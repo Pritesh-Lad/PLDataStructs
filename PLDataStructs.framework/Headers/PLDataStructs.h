9ee94381f6ac4a7f713eab72725e69c8edc08e2f
Pritesh Lad <priteshvlad@gmail.com>
2020-09-20 18:35:44 +0530

    add built framework

---
+++ PLDataStructs.framework/Headers/PLDataStructs.h
@@ -0,0 +1,19 @@
+//
+//  PLDataStructs.h
+//  PLDataStructs
+//
+//  Created by Pritesh on 19/09/20.
+//  Copyright © 2020 pritesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PLDataStructs.
+FOUNDATION_EXPORT double PLDataStructsVersionNumber;
+
+//! Project version string for PLDataStructs.
+FOUNDATION_EXPORT const unsigned char PLDataStructsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PLDataStructs/PublicHeader.h>
+#import <PLDataStructs/PLOrderedBTree.h>
+
